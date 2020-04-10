16abe87221b5014d3c635bc1325fce456a2620cc
Matej <matej.butkovic@gmail.com>
2020-04-10 20:26:04 +0200

    functioning reusable html snippets

--- htmIncluder.js
+++ htmIncluder.js
@@ -1,27 +1,83 @@
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
+$(document).ready(() => {
+    let z, i, elmnt, file, xhttp;
     /* Loop through a collection of all HTML elements: */
     z = document.getElementsByTagName("*");
     for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("w3-include-html");
-      if (file) {
-        /* Make an HTTP request using the attribute value as the file name: */
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            /* Remove the attribute, and call this function once more: */
-            elmnt.removeAttribute("w3-include-html");
-            includeHTML();
-          }
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("my_include_html");
+        let active_link = elmnt.getAttribute("active_link");
+        if (file) {
+            elmnt.innerHTML = snippets[file];
+            if (active_link) {
+                let elements = $(".nav").find("li");
+                for (j in elements) {
+                    console.log(elements[j].textContent);
+                    if (elements[j].textContent == active_link)
+                        $(elements[j]).addClass("active_li");
+                }
+
+
+
+            }
+
+            
         }
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /* Exit the function: */
-        return;
-      }
     }
-  }
+});
+
+let snippets = {
+    'nav': `
+<h4>NAVIGATION</h4>
+<nav role="navigation" class="navbar">
+  <ul class="nav navbar-nav">
+    <li><a href="index.html">Home</a></li>
+    <li><a href="contact.html">Contact</a></li>
+    <li><a href="blog.html">Articles</a></li>
+  </ul>
+</nav>
+<footer>
+  <h1 class="footer_title">Far</h1>
+  <h1 class="footer_title">More</h1>
+  <h1 class="footer_title">Different</h1>
+  <p>currently studying @ FER</p>
+</footer>
+`,
+    'article_nav': `
+<h4>NAVIGATION</h4>
+<nav role="navigation" class="navbar">
+  <ul class="nav navbar-nav">
+    <li><a href="../index.html">Home</a></li>
+    <li><a href="../contact.html">Contact</a></li>
+    <li><a href="../blog.html">Articles</a></li>
+  </ul>
+</nav>
+<footer>
+  <h1 class="footer_title">Far</h1>
+  <h1 class="footer_title">More</h1>
+  <h1 class="footer_title">Different</h1>
+  <p>currently studying @ FER</p>
+</footer>
+`,
+    'sidebar': `
+<p>A couple of articles explaining the apps are coming!</p>
+
+<div class="single_article">
+    <h3><a href="blog/2015-summer-project.html">My 2015 summer project</a></h3>
+    <p>The Pomodoro webapp</p>
+    <div><span>2.9.2019.</span></div>
+</div>
+
+<div class="single_article">
+    <h3><a href="blog/Reversed_Citations_Needed_app.html">Reversed Citations Needed
+    app</a></h3>
+    <p>
+    Making a simple PWA in a week: how to quickly get form idea to execution.
+
+    </p>
+
+    <div><span>12.9.2019.</span></div>
+</div>
+
+`
+};
