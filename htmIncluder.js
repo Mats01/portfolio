ff9a15b182b52cbf434e6495f72c808fce95e37f
Matej <matej.butkovic@gmail.com>
2020-04-11 13:09:41 +0200

    bigger mobile font

--- htmIncluder.js
+++ htmIncluder.js
@@ -79,5 +79,25 @@ let snippets = {
     <div><span>12.9.2019.</span></div>
 </div>
 
+`,
+'sidebar_blog': `
+<p>A couple of articles explaining the apps are coming!</p>
+
+<div class="single_article">
+    <h3><a href="2015-summer-project.html">My 2015 summer project</a></h3>
+    <p>The Pomodoro webapp</p>
+    <div><span>2.9.2019.</span></div>
+</div>
+
+<div class="single_article">
+    <h3><a href="Reversed_Citations_Needed_app.html">Reversed Citations Needed
+    app</a></h3>
+    <p>
+    Making a simple PWA in a week: how to quickly get form idea to execution.
+
+    </p>
+
+    <div><span>12.9.2019.</span></div>
+</div>
 `
 };
