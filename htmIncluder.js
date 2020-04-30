b22b27141f0be633356db459826bc6aead22cb3e
Matej <matej.butkovic@gmail.com>
2020-04-30 18:03:30 +0200

    rename articles to stories

--- htmIncluder.js
+++ htmIncluder.js
@@ -33,7 +33,7 @@ let snippets = {
   <ul class="nav navbar-nav">
     <li><a href="index.html">Home</a></li>
     <li><a href="contact.html">Contact</a></li>
-    <li><a href="blog.html">Articles</a></li>
+    <li><a href="blog.html">Stories</a></li>
   </ul>
 </nav>
 <footer>
@@ -49,7 +49,7 @@ let snippets = {
   <ul class="nav navbar-nav">
     <li><a href="../index.html">Home</a></li>
     <li><a href="../contact.html">Contact</a></li>
-    <li><a href="../blog.html">Articles</a></li>
+    <li><a href="../blog.html">Stories</a></li>
   </ul>
 </nav>
 <footer>
@@ -60,16 +60,16 @@ let snippets = {
 </footer>
 `,
     'sidebar': `
-<p>A couple of articles explaining the apps are coming!</p>
+<p>A couple of stories explaining the apps are coming!</p>
 
 <div class="single_article">
-    <h3><a href="blog/2015-summer-project.html">My 2015 summer project</a></h3>
+    <h3><a href="stories/2015-summer-project.html">My 2015 summer project</a></h3>
     <p>The Pomodoro webapp</p>
     <div><span>2.9.2019.</span></div>
 </div>
 
 <div class="single_article">
-    <h3><a href="blog/Reversed_Citations_Needed_app.html">Reversed Citations Needed
+    <h3><a href="stories/Reversed_Citations_Needed_app.html">Reversed Citations Needed
     app</a></h3>
     <p>
     Making a simple PWA in a week: how to quickly get form idea to execution.
@@ -81,7 +81,7 @@ let snippets = {
 
 `,
 'sidebar_blog': `
-<p>A couple of articles explaining the apps are coming!</p>
+<p>A couple of stories explaining the apps are coming!</p>
 
 <div class="single_article">
     <h3><a href="2015-summer-project.html">My 2015 summer project</a></h3>
